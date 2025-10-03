5070e0c2f0c6ff8716868e6aaa8b2713830c2d03
ocanath <ocanath@gmail.com>
2025-10-03 13:09:46 -0700

    single trigger now exits upon a successful trigger - i.e. it waits for data to be available for read

--- single_trigger.py
+++ single_trigger.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 from ds1202 import connect_to_scope
+import time
 
 def main():
     parser = argparse.ArgumentParser(description='Wrapper script for ds_1202_read_full function')
@@ -13,6 +14,17 @@ def main():
         rm, scope = connect_to_scope(args.ip_address)
         print("Single trigger now...")
         scope.write(":SINGle")
+        time.sleep(0.001)
+        print("Waiting for trigger to stop (no timeout!)")
+        while(1):
+            stat = scope.query(":TRIGger:STATus?")
+            if(stat.strip() == "STOP"):
+                break
+            time.sleep(0.1)
+            #if(stat.strip() != "STOP"):
+                #raise RuntimeError("Scope must be stopped before reading data")
+        print("Stopped.")
+
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
